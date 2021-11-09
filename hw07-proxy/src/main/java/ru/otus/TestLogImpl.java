97d2c830cc867e7cb05685070530132bb4467c36
Elizaveta.Prokhorova <prelizaweta@yandex.ru>
2021-11-09 21:23:29 +0300

    HW07. Corrections

--- hw07-proxy/src/main/java/ru/otus/TestLogImpl.java
+++ hw07-proxy/src/main/java/ru/otus/TestLogImpl.java
@@ -1,15 +1,11 @@
 package ru.otus;
 
-import ru.otus.annotations.Log;
-
 public class TestLogImpl implements TestLog {
-    @Log
     @Override
     public void calculation(int value) {
         System.out.println("One parameter");
     }
 
-    @Log
     @Override
     public void calculation(int a, int b) {
         System.out.println("Two parameters");
